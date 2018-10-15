680b29203e31d1e7c18dc0bdbdbf06ad508f042d
vishwanath mane email = vishwanath-govind.mane@capgemini.com <vishwman@LIN40000108.corp.capgemini.com>
2018-10-15 18:05:04 +0530

    change the data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into friendmanagement(email, friend_list, subscriber, subscribedBy, updated, updated_timestamp) values('john@example.com', '', '', '', '', now());
-insert into friendmanagement(email, friend_list, subscriber, subscribedBy, updated, updated_timestamp) values('andy@example.com', '', '', '', '', now());
-insert into friendmanagement(email, friend_list, subscriber, subscribedBy, updated, updated_timestamp) values('lily@example.com', '', '', '', '', now());
-insert into friendmanagement(email, friend_list, subscriber, subscribedBy, updated, updated_timestamp) values('lucy@example.com', '', '', '', '', now()); 
+insert into friendmanagement(email, friend_list, subscriber, subscribedBy, updated, updated_timestamp) values('vishu@gmail.com', '', '', '', '', now());
+insert into friendmanagement(email, friend_list, subscriber, subscribedBy, updated, updated_timestamp) values('prashant@gmail.com', '', '', '', '', now());
+insert into friendmanagement(email, friend_list, subscriber, subscribedBy, updated, updated_timestamp) values('ashu@gmail.com', '', '', '', '', now());
+insert into friendmanagement(email, friend_list, subscriber, subscribedBy, updated, updated_timestamp) values('gaurav@gmail.com', '', '', '', '', now()); 
 
-insert into unsubscribe(Requestor_email, Target_email, Subscription_Status) values('john@example.com', 'andy@example.com', 'Blocked');
-insert into unsubscribe(Requestor_email, Target_email, Subscription_Status) values('lily@example.com', 'lucy@example.com', 'Blocked');
+insert into unsubscribe(Requestor_email, Target_email, Subscription_Status) values('ashu@example.com', 'andy@gmail.com', 'Blocked');
+insert into unsubscribe(Requestor_email, Target_email, Subscription_Status) values('gaurav@example.com', 'lucy@gmail.com', 'Blocked');
